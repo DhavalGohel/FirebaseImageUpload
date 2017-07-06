bcc166eb0429c8e26689ce9dbd5f86c202a1b842
Nayan <nayan.dabhi@sphererays.net>
2017-07-06 11:07:41 +0530

    Client group service add

---
+++ src/providers/client-group/client-group-service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { AppConfig } from '../AppConfig';
+
+@Injectable()
+export class ClientGroupService {
+
+  constructor(
+    public http: Http,
+    public appConfig: AppConfig
+  ) {
+  }
+
+  getClientGroupList(token?: string, options?: RequestOptions) {
+    if (!options) {
+      options = new RequestOptions();
+    }
+
+    return new Promise(resolve => {
+      this.http.get(this.appConfig.API_URL + 'v1/ca/dashboard?api_token=' + token, options)
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data);
+        }, (err) => {
+          resolve(err.json());
+        });
+    });
+  }
+
+}
